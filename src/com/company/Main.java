52a58af5f537561c0c6560e4d7c829aa43f53b33
Inessa <i.vasileva.stc@innopolis.university.ru>
2020-12-04 12:42:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double price = 42.50;
+	int volume =50;
+
+	double money = price*volume;
+	System.out.println(money);
+
+	int salary=70000;
+	double ndfl=0.13;
+	double exit;
+	exit=salary-(ndfl*salary);
+	System.out.println(exit);
+
+	int sec=3600;
+	int hour;
+	hour=sec/3600;
+	System.out.println(hour);
+
+    }
+}
